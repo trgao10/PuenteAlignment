98329c1af2bb2f6ea6bd4e4b5854273479525b64
Tingran Gao <trgao10@math.duke.edu>
2015-06-23 21:54:41 -0700

    test

--- code/clusterMapLowRes.m
+++ code/clusterMapLowRes.m
@@ -43,7 +43,7 @@ for ii = 1 : ds.n
     progressbar(ii, ds.n, 20);
     [ds.shape{ ii }.origV, ds.shape{ ii }.origF] = read_off([meshesPath ds.names{ii} suffix]);
     ds.shape{ ii }.X              = cell( 1, ds.K );
-    ds.shape{ ii }.X{ ds.K }      = get_subsampled_shape( [outputPath 'subsampled', ds.ids{ii} , ds.N( ds.K )  );
+    ds.shape{ ii }.X{ ds.K }      = get_subsampled_shape( outputPath, ds.ids{ii} , ds.N( ds.K ) );
     ds.shape{ ii }.center         = mean(  ds.shape{ ii }.X{ ds.K }, 2 );
     ds.shape{ ii }.scale          = scale( ds.shape{ ii }.X{ ds.K } );
     ds.shape{ ii }.epsilon        = zeros( 1, ds.K );
