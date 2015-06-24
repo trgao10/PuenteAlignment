82a590b8f9fbe95d764fbb345053d8289b51a2aa
Tingran Gao <trgao10@math.duke.edu>
2015-06-23 22:09:55 -0700

    test

--- code/submit_jobs.m
+++ code/submit_jobs.m
@@ -4,8 +4,8 @@ function submit_jobs( pa, n_jobs )
 % Text wrappers
 PBS        = '#PBS -l nodes=1:ppn=1,walltime=3:00:00\n#PBS -m abe\n';
 script     = 'matlab -nodesktop -nodisplay -nojvm -nosplash -r ' ;
-matlab_cmd = @( kk ) ['\"cd ' pa.pfj '../../../code/;' 'jadd_path; load(''' pa.pfj 'job_' num2str( kk, '%.4d') ''');process_job(''' pa.pfj 'job_' num2str(kk,'%.4d') ''', ''' pa.pfj 'ans_' num2str(kk,'%.4d') ''', ''' pa.pfj 'f.mat'');exit;\"'];
-sub_sh     = @( kk ) ['!qsub -m e -M jpuente@math.princeton.edu -N job_' num2str(kk,'%.4d') ' -o ' pa.pfj 'stdout_' num2str(kk,'%.4d') ' -e ' pa.pfj 'stderr_' num2str(kk,'%.4d') ' ' pa.pfj 'job_' num2str(kk,'%.4d') '.sh'];
+matlab_cmd = @( kk ) ['\"cd ' pa.codePath ';' 'jadd_path; load(''' pa.pfj 'job_' num2str( kk, '%.4d') ''');process_job(''' pa.pfj 'job_' num2str(kk,'%.4d') ''', ''' pa.pfj 'ans_' num2str(kk,'%.4d') ''', ''' pa.pfj 'f.mat'');exit;\"'];
+sub_sh     = @( kk ) ['!qsub -m e -M trgao10@math.duke.edu -N job_' num2str(kk,'%.4d') ' -o ' pa.pfj 'stdout_' num2str(kk,'%.4d') ' -e ' pa.pfj 'stderr_' num2str(kk,'%.4d') ' ' pa.pfj 'job_' num2str(kk,'%.4d') '.sh'];
 
 %Create scripts
 for kk = 0 : n_jobs-1
