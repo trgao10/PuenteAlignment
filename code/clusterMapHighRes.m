a1549323dd1749ba93c0b88a8ad7762188f5101b
Tingran Gao <trgao10@math.duke.edu>
2015-06-23 22:46:46 -0700

    test

---
+++ code/clusterMapHighRes.m
@@ -0,0 +1,24 @@
+%% set path and preparation
+jadd_path;
+
+disp('Loading saved workspace...');
+load([outputPath 'session_low.mat']);
+disp('Loaded!');
+
+%% Compute the edges in the MST with higher number of points
+pa_tmp = localize( ga );
+pa.R = pa_tmp.R;
+
+k         = 2; % Which level to run next
+pa.A      = upper_triangle( ds.n );
+pa.pfj    = [ds.msc.output_dir 'jobs/']; % 'pfj' stands for path for jobs
+tmpR  = pa.R;
+tmpP  = pa.P;
+%dists = @(ii,jj) sqrt(sum((ds.shape{ii}.X{k-1}-tmpR{ii,jj}*ds.shape{jj}.X{k-1}*tmpP{ii,jj}).^2));
+%myM   = @(ii,jj)  (ds.shape{ jj }.neigh{k}' * pre_M( ds.shape{ii}.X{k-1} , ds.shape{jj}.X{k-1} , dists(ii,jj) , ds.shape{ii}.epsilon(k) , ds.shape{jj}.epsilon(k) ) * ds.shape{ ii }.neigh{ k });
+%f   = @( ii , jj ) locgpd(  ds.shape{ii}.X{k} , ds.shape{jj}.X{k}, pa.R{ii,jj} ,  myM(ii,jj)>0   );
+f   = @( ii , jj ) locgpd(  ds.shape{ii}.X{k} , ds.shape{jj}.X{k}, pa.R{ii,jj} ,  ones(ds.N(k))   );
+
+% Remember to remove all previous jobs in the output/jobs folder!
+system(['rm -rf ' pa.pfj '/*']);
+pa = compute_alignment( pa, f, n_jobs, use_cluster );
--- code/clusterReduceLowRes.m
+++ code/clusterReduceLowRes.m
@@ -16,8 +16,8 @@ plot_tree( pa.d+pa.d', mst, ds.names, 'mds', ones(1,ds.n),'');
 
 %% Output low resolution
 theta = pi/2; % Useful for rotating files to look nicer
-write_off_global_alignment( [ds.msc.output_dir 'alignment.off' ], ds , ga , 1:ds.n, 10, [cos(theta) -sin(theta) 0 ; sin(theta) cos(theta) 0; 0 0 1]*[ 0 0 1; 0 -1 0; 1 0 0]*ds.shape{1}.U_X{k}', 3.0, 1);
-write_morphologika( [ds.msc.output_dir 'morphologika_unscaled.txt' ], ds, ga );
+write_off_global_alignment( [ds.msc.output_dir 'alignment_low.off' ], ds, ga, 1:ds.n, 10, [cos(theta) -sin(theta) 0 ; sin(theta) cos(theta) 0; 0 0 1]*[ 0 0 1; 0 -1 0; 1 0 0]*ds.shape{1}.U_X{k}', 3.0, 1);
+write_morphologika( [ds.msc.output_dir 'morphologika_unscaled_low.txt'], ds, ga );
 
 disp('Saving current workspace....');
 system(['rm -rf ' outputPath 'session_low.mat']);
