4b6cf2805cd6f4998437263c601165de122f1620
Tingran Gao <trgao10@math.duke.edu>
2015-06-24 20:37:11 -0700

    add control for maximum number of iterations in each pairwise alignment

--- code/clusterMapLowRes.m
+++ code/clusterMapLowRes.m
@@ -82,7 +82,7 @@ end
 k = 1;
 pa.A          = upper_triangle( ds.n ); % a 1 entry in this matrix indicates the pairwise distance should be computed
 pa.L          = 8; % Number of positions to test, the first 8 are the 8 possibilities for aligning the principal axes
-f             = @( ii , jj ) gpd(  ds.shape{ii}.X{k}, ds.shape{jj}.X{k}, pa.L );
+f             = @( ii , jj ) gpd(  ds.shape{ii}.X{k}, ds.shape{jj}.X{k}, pa.L, pa.max_iter );
 pa.pfj        = [ds.msc.output_dir 'jobs/low/'];
 pa.codePath   = codePath;
 pa.max_iter   = max_iter;
